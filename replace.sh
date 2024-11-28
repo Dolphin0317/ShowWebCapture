a071e34ce205fd4d8ff8d833b092a7a4a1faf9ea
KevinLin <dolphin0317@gmail.com>
2024-11-28 15:35:26 +0800

    Create replace.sh

---
+++ replace.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 定義檔案
+index_file="index_original.html"
+file_list="file-list.txt"
+output_file="index.html"
+
+# 使用 sed 替換內容
+sed "/<!-- file-list.html 的內容將自動插入至此處 -->/ {
+    r ${file_list}
+    d
+}" "${index_file}" > "${output_file}"
+
+echo "已生成更新後的檔案：${output_file}"
